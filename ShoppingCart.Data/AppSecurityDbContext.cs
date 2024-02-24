63e0ed4762b6d51fb6c0673236b2e433f6e37079
Avinash Nagaiah <149421985+NagaiahAvinash@users.noreply.github.com>
2024-02-24 04:56:51 +0000

    minor changes

--- ShoppingCart.Data/AppSecurityDbContext.cs
+++ ShoppingCart.Data/AppSecurityDbContext.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace ShoppingCart.Data
 {
-    public class AppSecurityDbContext : IdentityDbContext
+    public class AppSecurityDbContext : IdentityDbContext<IdentityUser>
     {
         public AppSecurityDbContext(DbContextOptions<AppSecurityDbContext> options) : base(options) { }
     }
